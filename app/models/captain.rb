064d1a6a1fc8c81b8a858b72236d2ad2754b46e6
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-16 14:34:18 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -14,6 +14,6 @@ class Captain < ActiveRecord::Base
   end
 
   def self.non_sailors
-    includes(boats: :classifications).where.not(classifications: { name: 'Sailboat' })
+    all.pluck(:id).includes(boats: :classifications).where.not(classifications: { name: 'Sailboat' }).uniq
   end
 end
