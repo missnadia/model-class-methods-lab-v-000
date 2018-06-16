8b77f5161d9923d1b5fae124ecddf4f0c59d415c
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-16 14:47:08 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -13,6 +13,14 @@ class Captain < ActiveRecord::Base
     includes(boats: :classifications).where(classifications: { name: 'Sailboat' }, classifications: { name: 'Motorboat' })
   end
 
+  def self.sailboat_operators
+    includes(boats: :classifications).where(classifications: { name: 'Sailboat' }).uniq
+  end
+
+  def self.motorboat_operators
+    includes(boats: :classifications).where(classifications: { name: 'Motorboat' }).uniq
+  end
+
   def self.non_sailors
     includes(boats: :classifications).where.not(classifications: { name: 'Sailboat' }).pluck(:name).uniq
   end
